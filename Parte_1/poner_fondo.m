6d72fbf22c0baa512859d5e069d50d66eba62f2b
Edgar <edjchg@gmail.com>
2021-06-09 23:01:22 -0600

    Part 1 done

--- Parte_1/poner_fondo.m
+++ Parte_1/poner_fondo.m
@@ -1,15 +1,22 @@
-function C = poner_fondo(A,B,tol)
+function C = poner_fondo(A,B,tol, solo_verde)
   [m n z] = size(B);
   C=zeros(m,n,z);
-  for i=1:m
-    for j=1:n
-      rango=floor(255-255*tol);
-      if (0 <= A(i,j,1) && A(i,i,1) <= rango) && (rango <= A(i,j,2) && A(i,j,2) <= 255) && (0 <= A(i,j,3) && A(i,j,3) <= -(rango-255))
-        C(i,j,:) = B(i,j,:);
-      else
-        C(i,j,:) = A(i,j,:);
-      endif
-    endfor
-  endfor
+  a=zeros(m,n,z);
+  rango=round(255-255*tol);
+  % Analizando el canal rojo:
+  a(:,:,1) = and(0 <= A(:,:,1), A(:,:,1) <= rango); 
+  % Analizando el canal verde:
+  a(:,:,2) = and(rango <= A(:,:,2), A(:,:,2) <= 255); 
+  % Analizando el canal azul:
+  a(:,:,3) = and(0 <= A(:,:,3), A(:,:,3) <= rango);
+  a = and(a(:,:,1), a(:,:,2), a(:,:,3), a);
+  % Los indices que coinciden con el anterior analisis entonces son
+  % tomados del fondo para ponerlos en la imagen resultante.
+  C(a) = B(a);
+  if !solo_verde
+    % Los indices contrarios a los encontrados en el analisis anterior,
+    % son tomados de la figura para ponerlos en la imagen resultante.
+    C(~a) = A(~a);
+  endif
   C = uint8(C);
 endfunction
